82504447e80808dcb3ced049b52ded962e6aea3a
Oren Goldberg <ogoldberg@gmail.com>
2014-05-12 12:41:51 -0400

    enhanced testing with rake

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,22 +1,24 @@
 require_relative "test_helper"
+require_relative "test_controller"
 
 class TestApp < Ogeezy::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 
 class OgeezyAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def app 
+  def app
     TestApp.new
   end
 
   def test_request
-    get "/"
+    get "/example/route"
 
     assert last_response.ok?
     body = last_response.body
     assert body["Hello"]
-    assert body["Ogeezy"]
   end
-
 end
---
+++ test/test_controller.rb
@@ -0,0 +1,7 @@
+require_relative "test_helper"
+
+class TestController < Ogeezy::Controller
+  def index
+    "Hello!"
+  end
+end
